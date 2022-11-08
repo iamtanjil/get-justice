496e26aad66268eb9549a7cfbbfc9888e559f6a9
iamtanjil <tanjil4work@gmail.com>
2022-11-09 03:00:41 +0600

    work section final touch complete

--- src/Pages/Home/Methods/Methods.js
+++ src/Pages/Home/Methods/Methods.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import bg from '../../../assests/law-bg.jpg';
+import { FcCustomerSupport, FcBusinessContact, FcSurvey } from "react-icons/fc";
+
 
 const Methods = () => {
     return (
@@ -6,25 +9,43 @@ const Methods = () => {
             <h2 className='text-5xl text-center font-bold'>How Its Work</h2>
             <h2 className='text-2xl text-center font-bold'>Get your legal solution in 3 easy steps</h2>
             <div className='col-span-1 lg:grid grid-cols-3 lg:gap-4 m-7'>
-                <div className="card w-96 bg-base-100 shadow-xl image-full">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Speak to Our Team</h2>
-                        <p>Talk to our team of professionals to get a better understanding of your exact legal needs</p>
+                <div>
+                    <div className="badge badge-primary">Step-1</div>
+                    <div className="card w-96 bg-base-100 shadow-xl image-full">
+                        <figure><img src={bg} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <div className='felx mx-auto'>
+                                <FcCustomerSupport className='text-9xl'></FcCustomerSupport>
+                            </div>
+                            <h2 className="card-title justify-center text-center">Speak to Our Team</h2>
+                            <p className='text-center'>Talk to our team of professionals to get a better understanding of your exact legal needs</p>
+                        </div>
                     </div>
                 </div>
-                <div className="card w-96 bg-base-100 shadow-xl image-full">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title text-center">Connect with a Legal Service Provider</h2>
-                        <p>We connect you to a lawyer specialized to your case and assist you with putting together the required paperwork.</p>
+                <div>
+                    <div className="badge badge-primary">Step-2</div>
+                    <div className="card w-96 bg-base-100 shadow-xl image-full">
+                        <figure><img src={bg} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <div className='felx mx-auto'>
+                                <FcBusinessContact className='text-9xl'></FcBusinessContact>
+                            </div>
+                            <h2 className="card-title justify-center text-center">Connect with a Legal Service Provider</h2>
+                            <p className='text-center'>We connect you to a lawyer specialized to your case and assist you with putting together the required paperwork.</p>
+                        </div>
                     </div>
                 </div>
-                <div className="card w-96 bg-base-100 shadow-xl image-full">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body text-center">
-                        <h2 className="card-title ">Complete Your Legal Request</h2>
-                        <p>We see you through complete delivery of your required service until you are satisfied with our services</p>
+                <div>
+                    <div className="badge badge-primary">Step-3</div>
+                    <div className="card w-96 bg-base-100 shadow-xl image-full">
+                        <figure><img src={bg} alt="Shoes" /></figure>
+                        <div className="card-body text-center">
+                            <div className='felx mx-auto'>
+                                <FcSurvey className='text-9xl'></FcSurvey>
+                            </div>
+                            <h2 className="card-title justify-center text-center">Complete Your Legal Request</h2>
+                            <p className='text-center'>We see you through complete delivery of your required service until you are satisfied with our services</p>
+                        </div>
                     </div>
                 </div>
             </div>
